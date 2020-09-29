eb483fdf96ec43ca8b3723c0ae61f3e9cdfefa7d
sassquatch00 <justinawong.2019@sis.smu.edu.sg>
2020-09-30 00:44:30 +0800

    Implemented TransactionService and TransactionController

---
+++ src/main/java/cs203t10/ryver/fts/transaction/TransactionController.java
@@ -0,0 +1,36 @@
+package cs203t10.ryver.fts.transaction;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransactionController {
+  private TransactionService transactionService;
+
+  @Autowired
+  public TransactionController(TransactionService transactionService) {
+    this.transactionService = transactionService;
+  }
+
+  @GetMapping("/{accountId}/transactions")
+  public List<Transaction> getTransactionsById(@PathVariable long id) {
+    return transactionService.findById(id);
+  }
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping(value = {"/{senderAccountId}/transactions", "/{recevierAccountId}/transactions"})
+  public Transaction addTransaction(@PathVariable Long receiverAccountId, @PathVariable Long senderAccountId, @Valid @RequestBody Transaction transaction) {
+    Transaction savedTransaction = transactionService.addTransaction(transaction);
+    return savedTransaction;
+  }
+}
---
+++ src/main/java/cs203t10/ryver/fts/transaction/TransactionService.java
@@ -0,0 +1,34 @@
+package cs203t10.ryver.fts.transaction;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class TransactionService {
+  private TransactionRepository transactionRepository;
+
+  @Autowired
+  public TransactionService(TransactionRepository transactionRepository) {
+    this.transactionRepository = transactionRepository;
+  }
+
+  public Transaction addTransaction(Transaction transaction) {
+    return transactionRepository.save(transaction);
+  }
+
+  public List<Transaction> findByReceiverId(Long id) {
+    return transactionRepository.findByReceiverId(id);
+  }
+
+  public List<Transaction> findBySenderId(Long id) {
+    return transactionRepository.findBySenderId(id);
+  }
+
+  public List<Transaction> findById(long id) {
+    List<Transaction> transactionsById = new ArrayList<Transaction>(findByReceiverId(id));
+    transactionsById.addAll(findBySenderId(id));
+    return transactionsById;
+  }
+}
