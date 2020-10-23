3fcb642a7b7fdfd4ccae919bc380ebb0443e0137
christopherlim98 <christopherlim98@gmail.com>
2020-10-23 13:18:02 +0800

    Add bypass to h2-console

--- src/main/java/cs203t10/ryver/fts/security/SecurityConfig.java
+++ src/main/java/cs203t10/ryver/fts/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,6 +35,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
 	}
+
+	/**
+     *  Allow access to h2-console without http authorization
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers("/h2-console/**");
+    }
 
 	/**
 	 * Allow all requests by default, and control access with method security.
@@ -76,6 +87,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return source;
 	}
 
-	
+
 
 }
