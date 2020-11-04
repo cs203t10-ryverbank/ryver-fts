25ad5bce4a8f64291cb68169d0a41fd5877e7f1c
kohuining <huining.ko.2019@sis.smu.edu.sg>
2020-11-04 21:03:58 +0800

    Remove unused import

--- src/main/java/cs203t10/ryver/fts/auth/AuthService.java
+++ src/main/java/cs203t10/ryver/fts/auth/AuthService.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
