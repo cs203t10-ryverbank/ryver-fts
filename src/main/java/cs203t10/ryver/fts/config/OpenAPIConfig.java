56174923b4d46804a866ceebc4d05307dc203044
kohuining <huining.ko.2019@sis.smu.edu.sg>
2020-11-04 21:03:15 +0800

    Add OpenAPI config

---
+++ src/main/java/cs203t10/ryver/fts/config/OpenAPIConfig.java
@@ -0,0 +1,32 @@
+package cs203t10.ryver.fts.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Ryver Fund Transfer System (FTS)", version = "0.0.1", description = "The fund transfer service for Ryver Bank API"))
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class OpenAPIConfig {
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+            .group("Ryver FTS")
+            .pathsToMatch("/**")
+            .build();
+    }
+
+}
+
+
